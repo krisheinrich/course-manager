da3d44cd6ebfbc82ea3d565afad94ca44b9181f2
Kris Heinrich <krisheinrich@gmail.com>
2020-04-16 16:14:33 -0700

    Make CourseList component, wrap Home in AuthUserContext, pass authUser to CourseList and render courses for user

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,13 +1,77 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { compose } from 'recompose';
 
-import { withAuthorization, withEmailVerification } from '../Session';
+import {
+  AuthUserContext, withAuthorization, withEmailVerification
+} from '../Session';
+import { withFirebase } from '../Firebase';
+
+class _CourseList extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      loading: false,
+      courses: []
+    };
+  }
+
+  componentDidMount() {
+    const { uid: userId } = this.props.authUser;
+
+    this.setState({ loading: true }, () => {
+      this.props.firebase
+        .courses(userId)
+        .orderByChild('orderPosition')
+        .on('value', snapshot => {
+          const courses = [];
+          snapshot.forEach(child => {
+            courses.push({
+              id: child.key,
+              title: child.val().title
+            });
+          });
+
+          this.setState({
+            courses,
+            loading: false
+          });
+        }, err => {
+          this.setState({ loading: false });
+          console.error(err.code);
+        });
+    });
+  }
+
+  componentWillUnmount() {
+    const { uid: userId } = this.props.authUser;
+    this.props.firebase.courses(userId).off();
+  }
+
+  render() {
+    const { loading, courses } = this.state;
+
+    return loading
+      ? <div>Loading courses...</div>
+      : !courses.length
+        ? <div>Add a course to get started</div>
+        : <ul>
+            { courses.map(({ id, title }) => <li key={id}>{ title }</li>) }
+          </ul>;
+  }
+}
+
+const CourseList = withFirebase(_CourseList);
 
 const Home = () => (
-  <div>
-    <h1>Welcome to the Home page!</h1>
-    <p>The Home Page is accessible by every signed in user.</p>
-  </div>
+  <AuthUserContext.Consumer>
+  { authUser => (
+    <div>
+      <h1>Courses</h1>
+      <CourseList authUser={authUser}/>
+    </div>
+  )}
+  </AuthUserContext.Consumer>
 );
 
 const condition = authUser => !!authUser;
